de9f63ff4e849676164d331b1cd7ee64a90e3185
Johan Pålssonjoh <johan.palsson85@gmail.com>
2022-04-05 13:18:35 +0200

    Add project files.

---
+++ BBRG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BBRG.Startup))]
+namespace BBRG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
